3275733dd3aa8a95ea089850d9a9794ac22eb637
Jonathan Stowe <jns@gellyfish.co.uk>
2017-04-14 16:56:43 +0100

    Add the POD

--- lib/MessagePack/Class.pm
+++ lib/MessagePack/Class.pm
@@ -1,5 +1,104 @@
 use v6.c;
 
+=begin pod
+
+=head1 NAME
+
+MessagePack::Class - Serialize/Deserialize Perl 6 classes to/from MessagePack blobs
+
+=head1 SYNOPSIS
+
+=begin code
+
+use MessagePack::Class;
+
+class MyClass does MessagePack::Class {
+	has Str $.some-data;
+}
+
+my Blob $pack = MyClass.new(some-data => "whatever").to-messagepack;
+
+# Then send $pack over the network, write it to a file or something
+
+my MyClass $obj = MyClass.from-messagepack($pack);
+
+
+=end code
+
+=head1 DESCRIPTION
+
+L<MessagePack|http://msgpack.org/> is a binary serialization format that
+is particularly efficient for transmission over a network or file storage.
+
+This module provides a role that allows for the direct serialization of
+a Perl 6 object to a MessagePack binary blob and the deserialization of
+that blob back to a Perl 6 object of the same type with the same attribute
+values.
+
+Under the hood it uses L<Data::MessagePack|https://github.com/pierre-vigier/Perl6-Data-MessagePack>
+to serialize and deserialize data structures representing the object in a very
+similar manner to L<JSON::Marshal|https://github.com/jonathanstowe/JSON-Marshal> and
+L<JSON::Unmarshal|https://github.com/tadzik/JSON-Unmarshal> (infact it borrows some
+of the internal code of both of those to construct a suitable data structure.)
+
+For a simple case this may work with your class unchanged apart from the addition of
+the role composition, however for types that may not be properly constructed from
+their public attributes there are provided the attribute traits C<packed-by> and
+C<unpacked-by> which allow you to provide either a subroutine or a method name
+that will work with a representation that will round-trip properly.
+
+A named method supplied to C<packed-by> will be called on the object to be serialized
+without any arguments and should return a value suitable for serialization, and a method
+supplied to C<unpacked-by> will be called on the type object with the value to be
+deserialized as a single positional argument and should return an object of the type.
+
+
+So for instance if one had a class with an attribute of type Version one might do:
+
+=begin code
+
+class TraitTest does MessagePack::Class {
+    has Version $.version is packed-by('Str') is unpacked-by('new');
+}
+=end code
+
+Where the C<Str> method returns a string that is suitable to be passed to C<new>
+to create a new Version  object.
+
+If a subroutine (or other Callable object) is passed to the traits then it should take
+a single argument and return a value suitable for serialization (for C<packed-by>) or
+an object of the appropriate type (for C<unpacked-by>) so the above example might
+become:
+
+=begin code
+
+class TraitTest does MessagePack::Class {
+    has Version $.version is packed-by(-> Version $v { $v.Str }) is unpacked-by(-> Str $v { Version.new($v)});
+}
+
+=end code
+
+You can of course make the subroutines as complex as is required for your types.
+
+If you need your data to be interoperable with software written in another language
+you may need to adjust the serialization accordingly to match the types available
+in that language.
+
+=head1 METHODS
+
+=head2 method to-messagepack
+
+This should be called on an object of the consuming class and will return a C<Blob>
+containing the MessagePack data.
+
+=head2 method from-messagepack
+
+This should be called on the type object (i.e. a class method,) of the consuming
+class with a C<Blob> containing the MessagePack data that represents an object
+of this type, it returns a new initialised object.
+
+=end pod
+
 role MessagePack::Class {
     use Data::MessagePack;
 
